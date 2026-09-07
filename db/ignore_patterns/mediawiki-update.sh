07050cd91906b761752d0e08ff1560b728b168d7
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2026-09-07 22:07:58 +0000

    Bump MediaWiki version

--- db/ignore_patterns/mediawiki-update.sh
+++ db/ignore_patterns/mediawiki-update.sh
@@ -2,5 +2,5 @@
 for lang in Ar Bg Bs Ca Cs De El Eo Es Fa Fi Fr He It Ja Ka Ko Li Nl Pl Pt Ro Ru Sd Shn Sl Sq Sv Th Tr Uk Zh
 do
 	echo "${lang}"
-	./mediawiki-lang.py mediawiki.json <(curl --silent --location https://github.com/wikimedia/mediawiki/raw/1.34.2/languages/messages/Messages${lang}.php) mediawiki-${lang,,} >mediawiki-${lang,,}.json
+	./mediawiki-lang.py mediawiki.json <(curl --silent --location https://github.com/wikimedia/mediawiki/raw/1.46.0/languages/messages/Messages${lang}.php) mediawiki-${lang,,} >mediawiki-${lang,,}.json
 done
